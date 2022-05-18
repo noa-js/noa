572377b1b29440a4dee2877121c4972acaa03f9a
Choi-Jungwoo <jungwoo@cineuse.com>
2022-05-18 16:08:03 +0800

    feat: sync pro

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import i18n from './locales';
 import ElementPlus from 'element-plus';
 
 import 'element-plus/dist/index.css';
+import 'normalize.css';
+import 'reset.css';
+import 'animate.css';
 import './styles/index.scss';
 
-createApp(App).use(i18n).use(router).use(store).use(ElementPlus).mount('#app');
+createApp(App).use(i18n).use(router).use(ElementPlus).mount('#app');
